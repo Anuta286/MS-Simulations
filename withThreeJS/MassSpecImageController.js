d2d8a24ced480c33c9a8420ab0b5592dfd8e8cce
Anuta256 <annaponomarova256@gmail.com>
2024-02-19 16:28:00 +0100

    Add MassSpecImageController
    It makes main.js not so large.

---
+++ withThreeJS/MassSpecImageController.js
@@ -0,0 +1,54 @@
+import * as THREE from "three";
+import koefM2Px from "../withThreeJS/main.js";
+import {FieldShiningImage} from "./FieldShiningImage.js";
+
+export class MassSpecImageController {
+
+    /**
+     * @param massSpectrometer{MassSpectrometerTof}
+     * @param scene{Scene}
+     */
+    constructor(massSpectrometer, scene) {
+        this.massSpectrometer = massSpectrometer;
+        this.scene = scene;
+
+        let massSpecImageHeight = this.massSpectrometer.ionPulser.position.subtractVector(
+            this.massSpectrometer.mirror.position).getLength()*koefM2Px;
+        this.image = new THREE.Mesh(
+            new THREE.CylinderGeometry( 25, 25, massSpecImageHeight, 30, 30, false, 1.5, 3.14),
+            new THREE.MeshToonMaterial({color: 0xffffff, side: THREE.DoubleSide}));
+        this.image.rotation.z = Math.PI / 2;
+        this.image.position.set(
+            koefM2Px*(this.massSpectrometer.ionPulser.position.x+this.massSpectrometer.mirror.position.x)/2,
+            koefM2Px*massSpectrometer.ionPulser.position.y, -1);
+
+        this.fieldShining = new FieldShiningImage(this.image.position.x, this.image.position.y, this.image.position.z,
+            massSpecImageHeight, this.image.geometry.parameters.radiusTop*2);
+
+        let points = [];
+        points.push(new THREE.Vector3(-0.51*koefM2Px, -22, 5));
+        points.push(new THREE.Vector3(-0.51*koefM2Px, 23, 5));
+        this.fieldLine = new THREE.Line(new THREE.BufferGeometry().setFromPoints(points),
+            new THREE.LineBasicMaterial({color: 0xffffff}));
+
+        this.addAllElementsToScene();
+    }
+
+    addAllElementsToScene() {
+        this.scene.add(this.image);
+        this.scene.add(this.fieldShining.pulserFieldShining);
+        this.scene.add(this.fieldShining.mirrorFieldShining);
+        this.scene.add(this.fieldLine);
+    }
+
+    turnOffIonPulser() {
+        this.massSpectrometer.ionPulser.on = false;
+        this.scene.remove(this.fieldShining.pulserFieldShining);
+    }
+
+    turnOnIonPulser() {
+        this.massSpectrometer.ionPulser.on = true;
+        this.scene.add(this.fieldShining.pulserFieldShining);
+    }
+
+}
--- withThreeJS/main.js
+++ withThreeJS/main.js
@@ -3,7 +3,8 @@ import {Vector2D} from "../MathUtils/Vector2D.js";
 import {IonPulser} from "../MS/IonPulser.js";
 import {MassSpectrometerTof} from "../MS/MassSpectrometerTof.js";
 import {ParticleImage} from "./ParticleImage.js";
-import {FieldShiningImage} from "./FieldShiningImage.js";
+import {MassSpecImageController} from "./MassSpecImageController.js";
+
 
 let koefM2Px= 50; // 1px=0.02m //
 export default koefM2Px;
@@ -17,68 +18,43 @@ renderer.setPixelRatio(4);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let ionPulser = new IonPulser(10, 1, new Vector2D(-1.1, 0), true);
-let oppositeIonPulser = new IonPulser(10, 1, new Vector2D(1.1, 0), true);
-let massSpectrometer = new MassSpectrometerTof(ionPulser, oppositeIonPulser);
-
-let massSpecImageHeight = massSpectrometer.ionPulser.position.subtractVector(
-    massSpectrometer.mirror.position).getLength()*koefM2Px;
-const massSpecImage = new THREE.Mesh(
-    new THREE.CylinderGeometry( 25, 25, massSpecImageHeight, 30, 30, false,
-        1.5, 3.14), new THREE.MeshToonMaterial({color: 0xffffff, side: THREE.DoubleSide}));
-massSpecImage.rotation.z = Math.PI / 2;
-massSpecImage.position.set(koefM2Px*(massSpectrometer.ionPulser.position.x+massSpectrometer.mirror.position.x)/2,
-                           koefM2Px*massSpectrometer.ionPulser.position.y, -1);
-scene.add(massSpecImage);
-
-document.getElementById("button").addEventListener("click", ionPulserChangeState);
+let massSpecController = new MassSpecImageController(new MassSpectrometerTof(
+    new IonPulser(10, 1, new Vector2D(-1.1, 0), true),
+    new IonPulser(10, 1, new Vector2D(1.1, 0), true)), scene);
 
 const light = new THREE.HemisphereLight(0xffffff, 10, 3);
 light.position.set(20, 10, 10);
 scene.add(light);
-
-const points = []; //draw a line where the mirror stops working
-points.push(new THREE.Vector3(-0.51*koefM2Px, -22, 5));
-points.push(new THREE.Vector3(-0.51*koefM2Px, 23, 5));
-const line = new THREE.Line(new THREE.BufferGeometry().setFromPoints(points),
-    new THREE.LineBasicMaterial({color: 0xffffff}));
-scene.add(line);
-
-let fieldShining = new FieldShiningImage(massSpecImage.position.x, massSpecImage.position.y,
-    massSpecImage.position.z, massSpecImageHeight, massSpecImage.geometry.parameters.radiusTop*2);
-scene.add(fieldShining.pulserFieldShining);
-scene.add(fieldShining.mirrorFieldShining);
 
 let animationFrame = 0;
 function animate() {
     animationFrame++;
     requestAnimationFrame(animate);
     let deltaTime = 1e-13;
-    if (animationFrame > IonPulser.WORKING_TIME) {
-        ionPulser.on = false;
-        scene.remove(fieldShining.pulserFieldShining);
-    }
+    if (animationFrame > IonPulser.WORKING_TIME)
+        massSpecController.turnOffIonPulser();
     for (let particleImage of particlesArray) {
-        let particleNewPosition = massSpectrometer.getParticleNewPosition(particleImage.particle, deltaTime);
+        let particleNewPosition = massSpecController.massSpectrometer.getParticleNewPosition(particleImage.particle, deltaTime);
         particleImage.image.position.x = koefM2Px*particleNewPosition.x;
         particleImage.image.position.y = koefM2Px*particleNewPosition.y;
         particleImage.particle.position = particleNewPosition;
-        particleImage.particle.velocity = massSpectrometer.getParticleNewVelocity(particleImage.particle, deltaTime);
+        particleImage.particle.velocity = massSpecController.massSpectrometer.getParticleNewVelocity(particleImage.particle, deltaTime);
     }
     renderer.render(scene, camera);
 }
 
-function ionPulserChangeState() {
-    ionPulser.on = !ionPulser.on;
-    let state = ionPulser.on ? "ON" : "OFF";
+function ionPulserChangeState() { //make work with field-shining
+    massSpecController.massSpectrometer.ionPulser.on = !massSpecController.massSpectrometer.ionPulser.on;
+    let state = massSpecController.massSpectrometer.ionPulser.on ? "ON" : "OFF";
     document.getElementById("button").textContent = "IonPulser " + state;
 }
 
+document.getElementById("button").addEventListener("click", ionPulserChangeState);
 let ionPulserChargeInput = document.getElementById("ionPulserChargeInput");
-ionPulserChargeInput.addEventListener("keydown", function(event) {
+ionPulserChargeInput.addEventListener("keydown", function(event) { //regulate intensity for field-shining
     if (event.key === "Enter") {
         event.preventDefault();
-        ionPulser.charge = new Vector2D(ionPulserChargeInput.value, 0)
+        massSpecController.massSpectrometer.ionPulser.charge = new Vector2D(ionPulserChargeInput.value, 0)
     }
 });
 
@@ -95,9 +71,8 @@ particleMassesInput.addEventListener("keydown", function(event) {
         for (let particleImage of particlesArray)
             scene.add(particleImage.image);
         animationFrame = 0;
-        ionPulser.on = true;
-        oppositeIonPulser.on = true;
-        scene.add(fieldShining.pulserFieldShining);
+        massSpecController.massSpectrometer.mirror.on = true;
+        massSpecController.turnOnIonPulser();
         animate();
     }
 });
