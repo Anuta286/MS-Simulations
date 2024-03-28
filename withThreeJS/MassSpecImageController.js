fbf83d5e231f55e09a03c14eab7672277fd0ae54
Anuta256 <annaponomarova256@gmail.com>
2024-03-28 12:57:51 +0100

    Make constructor in MassSpecImageController simpler

--- withThreeJS/MassSpecImageController.js
+++ withThreeJS/MassSpecImageController.js
@@ -14,14 +14,25 @@ export class MassSpecImageController {
 
         let massSpecImageHeight = this.massSpectrometer.ionPulser.position.subtractVector(
             this.massSpectrometer.mirror.position).getLength()*koefM2Px;
+        this.initializeMS(massSpecImageHeight);
+        this.inilializeReflectron();
+        this.initializeFieldShining(massSpecImageHeight);
+        this.initializeFieldLine();
+
+        this.addAllElementsToScene();
+    }
+
+    initializeMS (massSpecImageHeight) {
         this.image = new THREE.Mesh(
             new THREE.CylinderGeometry( 25, 25, massSpecImageHeight, 30, 30, false, 1.5, 3.14),
             new THREE.MeshToonMaterial({color: 0xffffff, side: THREE.DoubleSide}));
         this.image.rotation.z = Math.PI / 2;
         this.image.position.set(
             koefM2Px*(this.massSpectrometer.ionPulser.position.x+this.massSpectrometer.mirror.position.x)/2,
-            koefM2Px*massSpectrometer.ionPulser.position.y, -1);
+            koefM2Px*this.massSpectrometer.ionPulser.position.y, -1);
+    }
 
+    inilializeReflectron() {
         let ringGeometry = new THREE.TorusGeometry(23.2, 0.7, 2, 100);
         let ringMaterial = new THREE.MeshToonMaterial({ color:  0x663300});
         this.reflectronRingImageArray = [];
@@ -31,17 +42,19 @@ export class MassSpecImageController {
             ring.position.set(27 + i*3, 0.5, -1);
             this.reflectronRingImageArray.push(ring);
         }
+    }
 
+    initializeFieldShining(massSpecImageHeight) {
         this.fieldShining = new FieldShiningImage(this.image.position.x, this.image.position.y, this.image.position.z,
             massSpecImageHeight, this.image.geometry.parameters.radiusTop*2);
+    }
 
+    initializeFieldLine() {
         let points = [];
         points.push(new THREE.Vector3(-0.51*koefM2Px, -22, 5));
         points.push(new THREE.Vector3(-0.51*koefM2Px, 23, 5));
         this.fieldLine = new THREE.Line(new THREE.BufferGeometry().setFromPoints(points),
             new THREE.LineBasicMaterial({color: 0xffffff}));
-
-        this.addAllElementsToScene();
     }
 
     addAllElementsToScene() {
